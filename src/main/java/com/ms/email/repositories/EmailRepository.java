b6ff54275ff946f6583479069ac14d87657fb29d
matheusgoes23 <matheusgoes23@gmail.com>
2022-01-06 14:19:58 -0300

    RabbitMQ messaging service implemented

--- src/main/java/com/ms/email/repositories/EmailRepository.java
+++ src/main/java/com/ms/email/repositories/EmailRepository.java
@@ -3,5 +3,7 @@ package com.ms.email.repositories;
 import com.ms.email.models.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<Email, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<Email, UUID> {
 }
