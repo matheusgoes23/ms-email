3d9ac1a314f0a58a73118cc43b225d6d7e89adb9
matheusgoes23 <matheusgoes23@gmail.com>
2022-01-06 21:06:06 -0300

    Features for getting data

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -5,13 +5,14 @@ import com.ms.email.models.Email;
 import com.ms.email.services.EmailService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 public class EmailController {
@@ -25,5 +26,15 @@ public class EmailController {
         BeanUtils.copyProperties(emailDTO, email);
         emailService.sendEmail(email);
         return new ResponseEntity<>(email, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/emails")
+    public ResponseEntity<Page<Email>> getAllEmails(Pageable pageable) {
+        return ResponseEntity.ok(emailService.findAll(pageable));
+    }
+
+    @GetMapping("/emails/{emailId}")
+    ResponseEntity<Email> getOneEmail(@PathVariable(value = "emailId") UUID emailId) {
+        return ResponseEntity.ok(emailService.findById(emailId));
     }
 }
--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -4,12 +4,15 @@ import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.Email;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class EmailService {
@@ -36,5 +39,13 @@ public class EmailService {
         } finally {
             return emailRepository.save(email);
         }
+    }
+
+    public Page findAll(Pageable pageable) {
+        return emailRepository.findAll(pageable);
+    }
+
+    public Email findById(UUID emailId) {
+        return emailRepository.findById(emailId).get();
     }
 }
